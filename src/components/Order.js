1f4de18a43b71fe9daaec428b848eac3681dc29c
Hanan88 <hanane.ahmed.8@hotmail.com>
2022-11-16 16:27:41 +0200

    update: order component

--- src/components/Order.js
+++ src/components/Order.js
@@ -4,17 +4,73 @@ const Order = ({ orders, setOrders, filterOrders, setFilterOrders }) => {
   const handleChange = (e) => {
     const { name, checked } = e.target;
     if (name === "allSelect") {
-      let tempOrder = orders.map((order) => {
+      let tempOrder = filterOrders.map((order) => {
         return { ...order, isChecked: checked };
       });
-      setOrders(tempOrder);
+      setFilterOrders(tempOrder);
     } else {
-      let tempOrder = orders.map((order) =>
+      let tempOrder = filterOrders.map((order) =>
         order.customerName === name ? { ...order, isChecked: checked } : order
       );
-      setOrders(tempOrder);
+      setFilterOrders(tempOrder);
     }
   };
+  const FilterData = filterOrders.map((order) => (
+    <tr key={order.id}>
+      <td scope="row">
+        <input
+          type="checkbox"
+          name={order.customerName}
+          onChange={handleChange}
+          checked={order?.isChecked || false}
+        />
+      </td>
+      <td>{order.id}</td>
+      <td>{order.orderDate}</td>
+      <td>{order.billName}</td>
+      <td>{order.shippingName}</td>
+      <td>{order.grandTotalBase}</td>
+      <td>{order.grandTotalPurchased}</td>
+      <td>{order.status}</td>
+      <td>{order.billAddress}</td>
+      <td>{order.shippingAddress}</td>
+      <td>{order.customerEmail}</td>
+      <td>{order.customerGroup}</td>
+      <td>{order.subTotal}</td>
+      <td>{order.shippingFees}</td>
+      <td>{order.customerName}</td>
+      <td>{order.paymentMethod}</td>
+      <td>{order.totalRefund}</td>
+    </tr>
+  ));
+  const RealData = orders.map((order) => (
+    <tr key={order.id}>
+      <td scope="row">
+        <input
+          type="checkbox"
+          name={order.customerName}
+          onChange={handleChange}
+          checked={order?.isChecked || false}
+        />
+      </td>
+      <td>{order.id}</td>
+      <td>{order.orderDate}</td>
+      <td>{order.billName}</td>
+      <td>{order.shippingName}</td>
+      <td>{order.grandTotalBase}</td>
+      <td>{order.grandTotalPurchased}</td>
+      <td>{order.status}</td>
+      <td>{order.billAddress}</td>
+      <td>{order.shippingAddress}</td>
+      <td>{order.customerEmail}</td>
+      <td>{order.customerGroup}</td>
+      <td>{order.subTotal}</td>
+      <td>{order.shippingFees}</td>
+      <td>{order.customerName}</td>
+      <td>{order.paymentMethod}</td>
+      <td>{order.totalRefund}</td>
+    </tr>
+  ));
   return (
     <div>
       <table className="table">
@@ -41,38 +97,7 @@ const Order = ({ orders, setOrders, filterOrders, setFilterOrders }) => {
             <th scope="col">total Refund</th>
           </tr>
         </thead>
-        <tbody>
-          {filterOrders.length > 0
-            ? filterOrders.map((order) => (
-                <tr key={order.id}>
-                  <td scope="row">
-                    <input
-                      type="checkbox"
-                      name={order.customerName}
-                      onChange={handleChange}
-                      checked={order?.isChecked || false}
-                    />
-                  </td>
-                  <td>{order.id}</td>
-                  <td>{order.orderDate}</td>
-                  <td>{order.billName}</td>
-                  <td>{order.shippingName}</td>
-                  <td>{order.grandTotalBase}</td>
-                  <td>{order.grandTotalPurchased}</td>
-                  <td>{order.status}</td>
-                  <td>{order.billAddress}</td>
-                  <td>{order.shippingAddress}</td>
-                  <td>{order.customerEmail}</td>
-                  <td>{order.customerGroup}</td>
-                  <td>{order.subTotal}</td>
-                  <td>{order.shippingFees}</td>
-                  <td>{order.customerName}</td>
-                  <td>{order.paymentMethod}</td>
-                  <td>{order.totalRefund}</td>
-                </tr>
-              ))
-            : null}
-        </tbody>
+        <tbody>{filterOrders ? FilterData : null}</tbody>
       </table>
       {/* Modal */}
       <div
