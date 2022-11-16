4648b89c1563eb3c6dc7a3e5d9e06ff2f94f4715
Hanan88 <hanane.ahmed.8@hotmail.com>
2022-11-16 09:53:03 +0200

    add: filters

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -6,23 +6,163 @@ const Filter = () => {
   const handleOpen = () => {
     setOpen(!open);
   };
-  const filterForm = "ss";
+  const filterForm = (
+    <div className="filter_box">
+      <div>
+        <label htmlFor="basic-url">Purchase Date from</label>
+        <input
+          type="date"
+          className="form-control"
+          id="basic-url"
+          aria-describedby="basic-addon3"
+        />
+      </div>
+
+      <div>
+        <label htmlFor="basic-url">Purchase Date to</label>
+        <input
+          type="date"
+          className="form-control"
+          id="basic-url"
+          aria-describedby="basic-addon3"
+        />
+      </div>
+      <div>
+        <label htmlFor="basic-url">Total base from</label>
+        <input
+          type="number"
+          className="form-control"
+          id="basic-url"
+          aria-describedby="basic-addon3"
+        />
+      </div>
+      <div>
+        <label htmlFor="basic-url">Total base to</label>
+        <input
+          type="number"
+          className="form-control"
+          id="basic-url"
+          aria-describedby="basic-addon3"
+        />
+      </div>
+      <div>
+        <label htmlFor="basic-url">Total Purchase from</label>
+        <input
+          type="number"
+          className="form-control"
+          id="basic-url"
+          aria-describedby="basic-addon3"
+        />
+      </div>
+      <div>
+        <label htmlFor="basic-url">Total Purchase to</label>
+        <input
+          type="number"
+          className="form-control"
+          id="basic-url"
+          aria-describedby="basic-addon3"
+        />
+      </div>
+      <div>
+        <label htmlFor="basic-url">subTotal from</label>
+        <input
+          type="number"
+          className="form-control"
+          id="basic-url"
+          aria-describedby="basic-addon3"
+        />
+      </div>
+      <div>
+        <label htmlFor="basic-url">subTotal to</label>
+
+        <input
+          type="number"
+          className="form-control"
+          id="basic-url"
+          aria-describedby="basic-addon3"
+        />
+      </div>
+      <div>
+        <label htmlFor="basic-url">Shipping cost from</label>
+
+        <input
+          type="number"
+          className="form-control"
+          id="basic-url"
+          aria-describedby="basic-addon3"
+        />
+      </div>
+
+      <div>
+        <label htmlFor="basic-url">Shipping cost to</label>
+
+        <input
+          type="number"
+          className="form-control"
+          id="basic-url"
+          aria-describedby="basic-addon3"
+        />
+      </div>
+      <div>
+        <label htmlFor="basic-url">Refund from</label>
+
+        <input
+          type="number"
+          className="form-control"
+          id="basic-url"
+          aria-describedby="basic-addon3"
+        />
+      </div>
+      <div>
+        <label htmlFor="basic-url">Refund to</label>
+        <input
+          type="number"
+          className="form-control"
+          id="basic-url"
+          aria-describedby="basic-addon3"
+        />
+      </div>
+      <div>
+        <label htmlFor="basic-url">Bill to name</label>
+        <input
+          type="number"
+          className="form-control"
+          id="basic-url"
+          aria-describedby="basic-addon3"
+        />
+      </div>
+      <div>
+        <label htmlFor="basic-url">Ship to name</label>
+        <input
+          type="number"
+          className="form-control"
+          id="basic-url"
+          aria-describedby="basic-addon3"
+        />
+      </div>
+
+      <div>
+        <label htmlFor="status">status</label>
+        <select name="status" id="status">
+          {}
+          <option value="volvo">Volvo</option>
+          <option value="saab">Saab</option>
+          <option value="mercedes">Mercedes</option>
+          <option value="audi">Audi</option>
+        </select>
+      </div>
+
+      {/* <div className="w-50 mb-3"></div> */}
+    </div>
+  );
 
   return (
-    <div className="w-75">
-      
-      <button
-        type="button"
-        className="btn btn-secondary"
-        data-bs-container="body"
-        data-bs-toggle="popover"
-        data-bs-placement="bottom"
-        data-bs-content="Bottom popover"
-      >
-        Popover on bottom
+    <div>
+      <button type="button" className="btn_filter" onClick={handleOpen}>
+        Filters
       </button>
 
-      {/* {open ? filterForm : null} */}
+      {open ? filterForm : null}
     </div>
   );
 };
